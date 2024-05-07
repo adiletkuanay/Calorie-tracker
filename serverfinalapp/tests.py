d47f69b54943d6bed164faea0a7394f8a69c9993
AdikВа <adiletkuanay@gmail.com>
2024-05-08 02:01:01 +0600

    Updated tests

--- serverfinalapp/tests.py
+++ serverfinalapp/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
 from django.contrib.auth.models import User
 from .models import Food, Consume
+from django.urls import reverse
 
 class FoodModelTest(TestCase):
     def setUp(self):
@@ -15,3 +16,19 @@ class FoodModelTest(TestCase):
         self.assertEqual(self.food.protein, 0.5)
         self.assertEqual(self.food.fats, 0.2)
         self.assertEqual(self.food.calories, 95)
+
+class HomeViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()  
+        self.user = User.objects.create_user(username="testuser", password="password")
+
+    def test_home_view_requires_login(self):
+        response = self.client.get(reverse("home")) 
+        self.assertEqual(response.status_code, 302)  
+        self.assertIn("/login/", response.url) 
+
+    def test_home_view_authenticated(self):
+        self.client.login(username="testuser", password="password")
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)  
+        self.assertTemplateUsed(response, "home.html")  
